94f201dc8f555a297bb755ea057059840f689b42
otmaaj <otmaaj@gmail.com>
2026-05-22 09:29:21 +0300

    update pydantic

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,6 +1,5 @@
 from pydantic import field_validator, BaseModel
 
-
 class UserCreate(BaseModel):
     name : str
     password : str
@@ -8,20 +7,29 @@ class UserCreate(BaseModel):
     @field_validator('name')
     @classmethod
     def valid_name(cls, v :str) -> str:
-        s = v.strip()
-        if len(s) < 3:
+        v = v.strip()
+        if len(v) < 3:
             raise ValueError('Имя пользователя должно содержать минимум 3 символа')
-        if len(s) > 28:
+        if len(v) > 28:
             raise ValueError('Имя пользователя слишком длинное')
-        return s
+        return v
+
 
     @field_validator('password')
     @classmethod
     def valid_pass(cls , v : str) -> str:
-        s = v.strip()
-        if len(s) < 8:
+        v = v.strip()
+        if len(v) < 8:
             raise ValueError('Пароль должен содержать минимум 8 символов')
-        return s
+
+        allowed = set(
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{};:'\",.<>/?\\|`~")
+
+        if not all(char in allowed for char in v):
+            raise ValueError('Пароль содержит недопустимые символы (только латиница, цифры и спецсимволы)')
+        return v
+
+
 
 class UserLogin(BaseModel):
     name: str
